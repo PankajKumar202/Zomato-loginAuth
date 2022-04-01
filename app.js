b55d5298c17c0321fac0e8e0e38ea6e80b0f6027
Pankaj Kumar <parya209@gmail.com>
2022-04-01 23:33:27 +0530

    final agian

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express=require('express');
 const app=express();
 const db=require('./db');
-const port=process.env.port || 4398;
+const port=process.env.port || 3200;
 const cors=require('cors');
 app.use(cors())
 const AuthController=require('./controller/authController');
-app.get('/',(req,res)=>{
-    res.send("Welcome to zomato login info")
- })
+
 app.use('/api/auth',AuthController);
 app.listen(port,()=>{
     console.log(`Running on port ${port}`)
